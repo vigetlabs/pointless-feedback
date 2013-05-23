8a884274d2f2e9047170d696d0564606fcb3c75d
Zachary Porter <zporter9@gmail.com>
2013-05-23 16:54:44 -0400

    Set flash notice default within I18n call
    Also, setup a method for `after_feedback_create_path`. This method will
    use the main app's root_path if defined, otherwise send the user to '/'.
    Eventually, we will let the main app override this method using the
    Decorator pattern.

--- app/controllers/pointless_feedback/feedbacks_controller.rb
+++ app/controllers/pointless_feedback/feedbacks_controller.rb
@@ -10,10 +10,19 @@ module PointlessFeedback
       @feedback = Feedback.new(params[:feedback])
 
       if @feedback.save
-        redirect_to '/', :notice => I18n.t('pointless_feedback.feedbacks.saved')
+        flash[:notice] = I18n.t('pointless_feedback.feedbacks.saved',
+                                :default => 'Thanks for your feedback!')
+
+        redirect_to after_feedback_create_path
       else
         render :new
       end
+    end
+
+    private
+
+    def after_feedback_create_path
+      main_app.respond_to?(:root_path) ? main_app.root_path : '/'
     end
   end
 end
