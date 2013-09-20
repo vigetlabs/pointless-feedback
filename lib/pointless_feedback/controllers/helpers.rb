386cc31582ba1bff80dae81bf97ed954646b9fb4
Patrick Reagan <reaganpr@gmail.com>
2013-09-20 16:03:55 -0600

    Prefer `try` to `respond_to?`

--- lib/pointless_feedback/controllers/helpers.rb
+++ lib/pointless_feedback/controllers/helpers.rb
@@ -3,7 +3,7 @@ module PointlessFeedback
     # Those helpers are convenience methods added to ApplicationController.
     module Helpers
       def after_message_create_path
-        main_app.respond_to?(:root_path) ? main_app.root_path : '/'
+        main_app.try(:root_path) || '/'
       end
     end
   end
