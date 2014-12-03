44b3f814e872138168b3e8879b7d4eb4aeffed80
Eli Fatsi <eli.fatsi@viget.com>
2014-12-03 14:50:00 -0700

    Can specify to send feedback emails from submitter

--- app/mailers/pointless_feedback/feedback_mailer.rb
+++ app/mailers/pointless_feedback/feedback_mailer.rb
@@ -1,13 +1,20 @@
 module PointlessFeedback
   class FeedbackMailer < ActionMailer::Base
-    default :from => PointlessFeedback.from_email
-
     def feedback(message)
       @message = message
-      mail(:to => to_emails, :subject => feedback_subject)
+
+      mail(:to => to_emails, :subject => feedback_subject, :from => from_address)
     end
 
     private
+
+    def from_address
+      if PointlessFeedback.send_from_submitter
+        @message.email_address
+      else
+        PointlessFeedback.from_email
+      end
+    end
 
     def to_emails
       Array(PointlessFeedback.to_emails)
