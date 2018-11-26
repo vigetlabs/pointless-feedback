83a299cbc802707b8a8c1c8428ffd3d7448cb402
Eli Fatsi <eli.fatsi@viget.com>
2018-11-26 13:14:42 -0700

    Added config details to README

--- lib/generators/templates/pointless_feedback.rb
+++ lib/generators/templates/pointless_feedback.rb
@@ -6,10 +6,17 @@ PointlessFeedback.setup do |config|
   # ==> Email Configuration
   # Configure feedback email properties (disabled by default)
   # Variables needed for emailing feedback
-  # config.email_feedback            = false
-  # config.send_from_submitter       = false
-  # config.from_email                = 'feedback@pointlesscorp.com'
-  # config.to_emails                 = ['first@example.com', 'second@example.com']
+  # config.email_feedback      = false
+  # config.send_from_submitter = false
+  # config.from_email          = 'feedback@pointlesscorp.com'
+  # config.to_emails           = ['first@example.com', 'second@example.com']
+
+  # ==> Google reCAPTCHA Configuration
+  # If you'd like to enable Google reCAPTCHA,
+  # 1. Register your site at: https://www.google.com/recaptcha/admin
+  # 2. !! Ensure you opt for reCAPTCHA v2. Support for v3 is not here yet.
+  # 3. Grab the site and secret key and paste them here.
+  #
   # config.google_captcha_site_key   = nil
   # config.google_captcha_secret_key = nil
 end
