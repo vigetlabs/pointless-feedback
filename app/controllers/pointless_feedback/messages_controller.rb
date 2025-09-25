af8bd5a5a60b00c753593ceaf805858f4e3244c8
DanielSBrown <danny.brown@viget.com>
2025-09-25 16:17:04 -0400

    Add rescue around create method

--- app/controllers/pointless_feedback/messages_controller.rb
+++ app/controllers/pointless_feedback/messages_controller.rb
@@ -9,15 +9,29 @@ module PointlessFeedback
     def create
       @message = Message.new(message_params)
 
-      if pass_captcha? && @message.save
-        flash[:notice] = I18n.t('pointless_feedback.messages.saved',
-                                :default => 'Thanks for your feedback!')
+      begin
+        captcha_passed = pass_captcha?
+        message_saved = @message.save if captcha_passed
 
-        redirect_to after_message_create_path
-      else
-        flash[:alert] = I18n.t('pointless_feedback.messages.invalid_captcha',
-                               :default => 'Are you a robot? Please check the box at the bottom of the page and try again.')
-        render :new
+        if captcha_passed && message_saved
+          flash[:notice] = I18n.t('pointless_feedback.messages.saved',
+                                  :default => 'Thanks for your feedback!')
+          redirect_to after_message_create_path
+        else
+          flash[:alert] = I18n.t('pointless_feedback.messages.invalid_captcha',
+                                 :default => 'Are you a robot? Please check the box at the bottom of the page and try again.')
+          render :new
+        end
+      rescue => e
+        Rails.logger.error "PointlessFeedback error: #{e.message}"
+        Rails.logger.error e.backtrace.join("\n")
+
+        if Rails.env.development?
+          raise e
+        else
+          flash[:alert] = "Sorry, there was a technical problem. Please try again later."
+          render :new
+        end
       end
     end
 
